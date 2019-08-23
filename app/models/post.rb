a83871b76a7e2a4b1c94f78e6565a932d4f8224e
Abe Petrillo <abe.petrillo@gmail.com>
2019-08-23 22:27:42 +0100

    Add featured image for menu

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,14 @@
 class Post < ApplicationRecord
   has_rich_text :content
+  has_one_attached :featured_image
 
   def summary
-    content.to_plain_text[0..50]
+    content.to_plain_text[0..100]
   end
+
+  def featured_image_url
+   if self.featured_image.attachment
+     self.featured_image.attachment.service_url
+   end
+ end
 end
