43c0309334eaa2388bf2b03f45d646bc85e29df3
Abe Petrillo <abe.petrillo@gmail.com>
2019-08-28 21:18:55 +0100

    try adding a border

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :validatable
+  options = [
+    :database_authenticatable, :recoverable, :rememberable, :validatable
+  ]
+  if Rails.env.development?
+    options.push :registerable
+  end
+  devise *options
 end
